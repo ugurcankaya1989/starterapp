21ee827ce724c81282a1ffd761320bf78562c9cf
Uğur Çankaya <>
2019-08-18 19:22:21 +0300

    Spring Boot starter with Rest app

--- src/main/java/com/galatasaray/starterapp/StarterappApplication.java
+++ src/main/java/com/galatasaray/starterapp/StarterappApplication.java
@@ -3,7 +3,7 @@ package com.galatasaray.starterapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+// New SpringBoot APP
 @SpringBootApplication
 public class StarterappApplication {
 
